672ac455861377807a36a3412160cdb951d2c25a
iwase22334 <iwase22334@gmail.com>
2022-09-26 17:41:34 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+## 参考
+- https://www.kenpakusha.co.jp/pdf/pdf_ee7.0_ryouri_ichiran.pdf
+- https://www.mext.go.jp/component/b_menu/shingi/toushin/__icsFiles/afieldfile/2010/11/16/1299052_3_1.pdf
+- 
---
+++ script/request-audio-generate.py
@@ -0,0 +1,49 @@
+import sys
+import json
+import requests
+
+
+def request_audio(text: str, speaker: str):
+    host = 'localhost'
+    port = 50031
+
+    params = {
+            'text': text,
+            'speaker': speaker,
+        }
+    print(params)
+
+    audio_query = requests.post(
+        f'http://{host}:{port}/audio_query',
+        params=params
+    )
+
+    print(audio_query.text)
+    print(json.dumps(audio_query.json()))
+
+    response_wav = requests.post(
+        f'http://{host}:{port}/synthesis',
+        headers={'Content-Type': 'application/json', },
+        params=params,
+        data=json.dumps(audio_query.json())
+    )
+
+    return response_wav
+
+
+if __name__ == "__main__":
+    # {
+    #  "name": "MANA+",
+    #  "speaker_uuid": "2932eb06-e388-45bf-a6ba-dbc66a48961e",
+    #  "styles": [
+    #    { "name": "ふくれっつら", "id": 41 },
+    #    { "name": "しょんぼり", "id": 42 }
+    #  ],
+    #  "version": "1.1.0"
+    # }
+
+    # sentence = sys.stdin.readline().rstrip('\n')
+    sentence = sys.argv[1]
+    response = request_audio(sentence, "42")
+    with open(f"{sentence}.wav", "wb") as file:
+        file.write(response.content)
